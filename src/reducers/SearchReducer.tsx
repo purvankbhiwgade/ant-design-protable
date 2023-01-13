2e81c70043803da6b2b07fd6a3e96ede1fd9ecdb
Purvank Bhiwgade <bhiwgadepurvank@gmail.com>
2023-01-13 11:35:29 +0530

    deleted unused files

--- src/reducers/SearchReducer.tsx
+++
@@ -1,9 +0,0 @@
-export const filterReducer = (state, action) => {
-  switch (action.type) {
-    case "FILTER_BY_SEARCH":
-      return { ...state, searchQuery: action.payload };
-
-    default:
-      return state;
-  }
-};
